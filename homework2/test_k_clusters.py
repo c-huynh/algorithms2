e3d56da44d4a6c6341918d0299aa127159875cb4
Chi Chi Huynh <chichi.huynh@outlook.com>
2021-04-30 00:35:39 -0400

    added k cluster test

--- homework2/test_k_clusters.py
+++ homework2/test_k_clusters.py
@@ -1,11 +1,11 @@
-# from k_clusters import *
-#
-# def test_small():
-#     file = "test_small.txt"
-#     k = 2
-#     assert largest_k_clusters(file, k) == 5
-#
-# def test_small():
-#     file = "test_small2.txt"
-#     k = 4
-#     assert largest_k_clusters(file, k) == 7
+from k_clusters import *
+
+def test_small():
+    file = "test_small.txt"
+    k = 2
+    assert largest_k_clusters(file, k) == 5
+
+def test_small():
+    file = "test_small2.txt"
+    k = 4
+    assert largest_k_clusters(file, k) == 7
